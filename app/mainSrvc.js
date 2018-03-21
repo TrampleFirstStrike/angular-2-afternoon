c041a39031336b9e23cabc27dbc3f181acb957a8
Jordan Oakes <jordanoakes@Jordans-MacBook-Air.local>
2018-03-21 15:34:50 -0500

    completed

--- app/mainSrvc.js
+++ app/mainSrvc.js
@@ -1,3 +1,15 @@
-angular.module('devmtnTravel').service('mainSrvc', function() {
-  
+angular.module('devmtnTravel').service('mainSrvc', function($http) {
+  this.getTravelInfo = function() {
+      return $http({
+          method: 'GET',
+          url: 'http://practiceapi.devmountain.com/api/travel/travel-info'
+      });
+    };
+
+  this.getPackagedInfo = function() {
+      return $http({
+          method: 'GET',
+          url: 'https://practiceapi.devmountain.com/api/travel/package-info'
+      });
+    };
 });
